a9f8a17f93bbcc453f5d4d5c42f331c5fd160374
MatheusGorga <matheusgobar@gmail.com>
2022-12-21 08:15:37 -0300

    instalando sass

--- src/components/cabecalho/Cabecalho.jsx
+++ src/components/cabecalho/Cabecalho.jsx
@@ -1,10 +1,16 @@
+import styles from './Cabecalho.module.scss';
+
 function Cabecalho() {
   return (
-    <header className=''>
+    <header className={styles.cabecalho}>
       <img src='/assets/logo.png' alt='' />
 
-      <div>
-        <input type='text' placeholder='O que voce procura' />
+      <div className={styles.cabecalho__container}>
+        <input
+          className={styles.cabecalho__input}
+          type='text'
+          placeholder='O que voce procura'
+        />
         <img src='/assets/search.png' alt='lupa' />
       </div>
     </header>
