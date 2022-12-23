3226f2e841233813a2bee7fd85653f3aa05c4a79
MatheusGorga <matheusgobar@gmail.com>
2022-12-23 17:34:35 -0300

    fix galeria

--- src/components/galeria/Galeria.jsx
+++ src/components/galeria/Galeria.jsx
@@ -1,12 +1,33 @@
 import Tags from '../tags/Tags';
 import styles from './Galeria.module.scss';
+import fotos from './fotos.json';
 
 function Galeria() {
   return (
     <div className={styles.galeria}>
       <h2>Navegue pela galeria</h2>
       <Tags />
-      <ul></ul>
+      <ul className={styles.galeria__cards}>
+        {fotos.map((foto) => {
+          return (
+            <li key={foto.id} className={styles.galeria__card}>
+              <img
+                className={styles.galeria__imagem}
+                src={foto.imagem}
+                alt={foto.titulo}
+              />
+              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <div>
+                <p>{foto.creditos}</p>
+                <span>
+                  <img src='/assets/favorito.png' alt='icone para curtir' />
+                  <img src='/assets/open.png' alt='icone para abrir modal' />
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
